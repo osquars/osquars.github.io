3e15f481be5996479bec66193f3dc9835cc6edfb
Osquars <mats@ringel.se>
2015-12-19 03:46:49 +0100

    Setting up repeat on tables for all weapons

--- assets/js/weaponCtrl.js
+++ assets/js/weaponCtrl.js
@@ -2,6 +2,7 @@
 app.controller('weaponCtrl', function($scope, $http) {
     $http.get($scope.hostUrl + 'assets/json/weapon-stats.json')
         .then(function(response) {
+            $scope.weaponList = response.data.weaponList;
             $scope.bwMelee = response.data.bwMelee;
             $scope.bwRange = response.data.bwRange;
         });
