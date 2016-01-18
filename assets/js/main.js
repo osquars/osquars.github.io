08d8522b82997d776cda1db6affb329d3ba704b6
Mats Ringel <mats@ringel.se>
2016-01-18 12:09:01 +0100

    Testing function to remove Hashtag for the pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ var app = angular.module('pageApp', ['ngRoute']);
 /**
  * Configure the Routes
  */
-app.config(['$routeProvider', function ($routeProvider) {
+app.config(['$routeProvider','$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider
     // Home
         .when("/", {
@@ -78,6 +78,18 @@ app.config(['$routeProvider', function ($routeProvider) {
             templateUrl: "partials/404.html",
             controller: "PageCtrl"
         });
+    //check browser support
+    if(window.history && window.history.pushState) {
+        //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+
+        // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+
+        // if you don't wish to set base URL then use this
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: true
+        });
+    }
 }]);
 
 app.run(['$rootScope', function($rootScope) {
